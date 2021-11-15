2de0c091eba9e2a0bc44d85da83f32e79fb87f8c
Wesley <wesleyadwebster@gmail.com>
2021-11-15 07:52:17 -0600

    Manual Acceptance Testing - imported our components in main App file to see it render

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
+import Congrats from './components/Congrats/Congrats';
+import GuessedWords from './components/GuessedWords/GuessedWords';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Congrats success={false} />
+      <GuessedWords 
+        secretWord={"party"}
+        success={true}
+        guessedWords={[ {guessedWord: 'train', letterMatchCount: 3}]} />
     </div>
   );
 }
