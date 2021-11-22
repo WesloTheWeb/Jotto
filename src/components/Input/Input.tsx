4d7448f1e1f0b0a8d8d95f7ecf2c85f128249f6b
Wesley <wesleyadwebster@gmail.com>
2021-11-22 00:19:52 -0600

    Implementation and styling of the input

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -13,16 +13,18 @@ const Input = (props: InputProps) => {
     }
 
     return (
-        <div data-test='component-input'>
-            <form>
+        <div data-test='component-input' className="m-8">
+            <form className="form-container">
                 <input
                     data-test="input-box"
+                    className="input-form-property"
                     type="text"
                     placeholder="enter guess"
                     value={currentGuess}
                     onChange={(evnt) => setCurrentGuess(evnt.target.value)} />
                 <button
                     data-test="submit-button"
+                    className="bg-green-600 p-1.5 font-bold"
                     onClick={() => {
                         setCurrentGuess('');
                     }}>
